695415ef7a8c3e03cec5f5e2d5e36ca889a4cc65
flowerz <itsmefl0w3rz@gmail.com>
2024-12-05 19:55:02 -0600

    rebrand because i got a domain :yay:

--- internal/config.php
+++ internal/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $site_url = 'localhost';
-$site_name = 'FuckWatch';
-$site_slogan = 'Wow, this shit sucks!';
+$site_name = 'ClipVerse';
+$site_slogan = 'Im gonna kill myself';
 $site_lightlogo = '/assets/img/temporary_logo_light.png';
 $site_darklogo = '/assets/img/temporary_logo_dark.png';
 $site_placeholderthumbnail = '/content/thumbnails/temporary_placeholder_thumbnail.png';
